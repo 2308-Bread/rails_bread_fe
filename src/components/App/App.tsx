588364d1adc96dc3addfdc3a3129924fc12c36ff
Captainlearyo <clayoleary@gmail.com>
2024-02-02 22:05:14 -0600

    this is very broken

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css'; // Import Leaflet CSS
+import 'leaflet/dist/leaflet.css';
 import './App.css';
+import CountriesMap from '../Map/Map';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <MapContainer center={[51.505, -0.09]} zoom={5} style={{ height: '900px', width: '100%' }}>
+        <MapContainer center={[51.505, -0.09]} zoom={3} style={{ height: '900px', width: '100%' }}>
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           />
-          {/* Add your choropleth layer here */}
+          <CountriesMap />
         </MapContainer>
       </header>
     </div>
@@ -20,3 +21,6 @@ function App() {
 }
 
 export default App;
+
+
+
--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,26 +1,54 @@
-import React, { Component } from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import React from 'react';
+import { MapContainer, GeoJSON } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import countriesData from './countries.geo.json';
 
-class CountriesMap extends Component {
-  render() {
-    return (
-      <div className="map-container">
-        <MapContainer
-          center={[0, 0]}
-          zoom={2}
-          style={{ height: '500px', width: '100%' }}
-        >
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          />
-          {/* Add country data here */}
-        </MapContainer>
-      </div>
-    );
-  }
+interface GeoJSONFeature {
+  type: 'Feature';
+  properties: {
+    [key: string]: any;
+  };
+  geometry: {
+    type: string;
+    coordinates: any;
+  };
 }
+
+interface GeoJSONData {
+  type: 'FeatureCollection';
+  features: GeoJSONFeature[];
+}
+
+const CountriesMap = () => {
+  const handleCountryClick = (event: any, feature: GeoJSONFeature) => {
+    alert(`Clicked on ${feature.properties.name}`);
+  };
+
+  const onEachCountryFeature = (feature: GeoJSONFeature, layer: any) => {
+    layer.on({
+      click: (event: any) => handleCountryClick(event, feature),
+    });
+  };
+
+  return (
+    <MapContainer
+      center={[0, 0]}
+      zoom={2}
+      style={{ height: '500px', width: '100%' }}
+    >
+      <GeoJSON
+        data={countriesData as GeoJSONData}
+        style={() => ({
+          fillColor: 'transparent',
+          color: 'blue',
+          weight: 1,
+          fillOpacity: 0.6,
+        })}
+        onEachFeature={onEachCountryFeature as any} // Specify type as any
+      />
+    </MapContainer>
+  );
+};
 
 export default CountriesMap;
 
